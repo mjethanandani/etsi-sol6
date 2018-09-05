eaa9e8a66691599d68ed035bfb9447f0b35c05b1
Fredrik Jansson <frjansso@cisco.com>
2018-09-05 10:56:32 -0700

    Fixed base container for test

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM fredrikjanssonse/etsi-sol006-base-image:6.6
+FROM fredrikjanssonse/etsi-sol006-base-image
 
 ADD example-data/* src/
 ADD packages/sol6/src/yang/* src/
--- run-test.sh
+++ run-test.sh
@@ -6,8 +6,17 @@ set -e
 
 source /opt/confd/confdrc
 cd src
+
+echo "Testing compilation"
 confdc -c --fail-on-warnings -o /opt/confd/etc/confd/etsi-nfv-common.fxs etsi-nfv-common.yang
 confdc -c --fail-on-warnings -o /opt/confd/etc/confd/etsi-nfv.fxs etsi-nfv.yang
+
+echo "Starting ConfD"
 confd
+
+echo "Loading Data"
 confd_load -l -m nfv.xml
 
+# Don't do this in the actual test, just waste of cycles
+# echo "Stopping ConfD"
+# confd --stop
